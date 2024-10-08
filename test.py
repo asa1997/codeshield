6b2277d1eb03e46ce8230fa5f801b8ee9b938dfd
Arun Suresh <58501088+asa1997@users.noreply.github.com>
2024-10-08 11:03:58 +0530

    updated

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import json
 
 class IBMGraniteLLM:
     def __init__(self):
-        self.model_name = "ibm-granite/granite-3b-code-base"
+        self.model_name = "codellama/CodeLlama-7b-Instruct-hf"
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
         self.model = AutoModelForCausalLM.from_pretrained(self.model_name)
 
