8ce2eb392b2f64d5bf88a2deba2961180f69db03
Arun Suresh <58501088+asa1997@users.noreply.github.com>
2024-10-08 12:34:14 +0530

    updated

--- test1.py
+++ test1.py
@@ -32,7 +32,7 @@ async def scan_llm_output(llm_output_code):
 
 class IBMGraniteLLM:
     def __init__(self):
-        self.model_name = "ibm-granite/granite-3b-code-base"
+        self.model_name = "Salesforce/codet5-base"
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
         self.model = AutoModelForSeq2SeqLM.from_pretrained(self.model_name)
 
